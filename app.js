158e00a7822834d8d47cc31badabd2fda96cda1c
klaudiusmiskinis <klaudiusmiskinis@gmail.com>
2021-09-17 12:24:52 +0200

    Cambios en rutas y añadido de Submit en los Forms

--- app.js
+++ app.js
@@ -5,13 +5,17 @@ app.get('/', (req, res) => {
     res.render('ejs/index.ejs', {name: 'Klau'})
 })
 
+app.use(express.urlencoded({ extended: false}))
 app.use(express.static('views'));
 app.use(express.static('assets'))
-
 app.set('set engine', 'ejs')
 
 app.get('/login', (req, res) => {
     res.render('ejs/login.ejs')
+})
+
+app.post('/register', (req, res) => {
+    req.body.name
 })
 
 app.get('/register', (req, res) => {
